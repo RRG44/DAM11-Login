eea30c976c558d243f198e843888df49d80c37a3
Gera <gr.misterg@gmail.com>
2024-03-06 19:16:27 -0600

    Work Finished

--- App.jsx
+++ App.jsx
@@ -1,49 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert, TouchableOpacity } from 'react-native';
-import { LogInButton } from './components/UIComponents.js'
+import { View, Text, StyleSheet, Alert, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import * as UI from './components/UIComponents.js'
 
 const App = () => {
-  const [expediente, setExpediente] = useState('');
+  const [id, setId] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = () => {
-    expediente.length == 6 ? 
-      (password.length == 8?
-        Alert.alert('LogIn exitoso'): Alert.alert('LogIn fallido', 'La constraseña debe tener al menos 8 caracteres.')) 
-    : Alert.alert('LogIn fallido', 'El expediente debe tener 6 caracteres.');
+    id.length == 6 ? 
+      (password.length >= 8?
+        Alert.alert('Log in success'): Alert.alert('Log in fail', 'Passwords must be at leat 8 characters long.')) 
+    : Alert.alert('Log in fail', 'IDs must be 6 characters long.');
   };
 
   return (
-    <View style = {styles.mainContainer}>
-      <View>
-        <Text>LogIn</Text>
+    <TouchableWithoutFeedback onPress = {() => Keyboard.dismiss()}>
+      <View style = {styles.mainContainer}>
+        <View style = {{marginTop: 200,}}>
+          <Text style = {styles.title}>Log in</Text>
+        </View>
+        <View style = {styles.inputContainer}>
+          <UI.LogInInputExpedient onChangeText = {setId} maxLength = {6}/>
+          <UI.LogInInputPassword onChangeText = {setPassword} />
+          <UI.LogInButton onPress={handleSubmit} />
+        </View>
       </View>
-      <View>
-        <TextInput
-          placeholder='Expediente'
-          value={expediente}
-          keyboardType='numeric'
-          maxLength={6}
-          multiline={false}
-          onChangeText={(text) => setExpediente(text)}
-        />
-        <TextInput
-          placeholder='Password'
-          multiline={false}
-          secureTextEntry={true}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <LogInButton onPress={handleSubmit} />
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   mainContainer: {
-    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'center',
+    width: '100%',
+  },
+  inputContainer:
+  {
+    margin: 40,
+    alignItems: 'center',
+    width: '100%',
+  },
+  title:
+  {
+    fontSize: 28,
+    fontWeight: 'bold',
   },
 });
 
--- components/UIComponents.js
+++ components/UIComponents.js
@@ -1,23 +1,72 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, TextInput, useState } from 'react-native';
 
 export const LogInButton = props => {
   return (
     <>
       <TouchableOpacity onPress={props.onPress} style = {styles.btnLogIn}>
-        <Text style = {styles.btnLogInText}>LogIn</Text>
+        <Text style = {styles.btnLogInText}>Log in</Text>
       </TouchableOpacity>
     </>
+  );
+};
+
+export const LogInInputExpedient = props => {
+  inputState = props.inputState;
+  return (
+    <TextInput
+      placeholder='ID'
+      multiline={false}
+      keyboardType='numeric'
+      maxLength={props.maxLength ? props.maxLength: 524288}
+      onChangeText={(text) => props.onChangeText(text)}
+      style = {styles.txtInput}
+    />
+  );
+};
+
+export const LogInInputPassword = props => {
+  return (
+    <TextInput
+      placeholder='Password'
+      multiline={false}
+      keyboardType='default'
+      maxLength={props.maxLength ? props.maxLength: 524288}
+      secureTextEntry={true}
+      onChangeText={(text) => props.onChangeText(text)}
+      style = {styles.txtInput}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   btnLogIn: 
   {
-    backgroundColor: 'blue'
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    padding: 10,
+    margin: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 60,
   },
   btnLogInText:
   {
     color: 'white',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  txtInput:
+  {
+    padding: 10,
+    margin: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    textAlign: 'left',
+    borderColor: '#aaa',
+    width: 300,
+    height: 60,
+    fontSize: 16,
   },
 });
